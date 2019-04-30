94476c0773a82770cdc3c0e0d89a18c483791233
lxx14 <lxx1427@gmail.com>
2019-04-30 13:19:57 +0300

    start developing favorite function

--- src/components/SinglePagePhoto/index.js
+++ src/components/SinglePagePhoto/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './style.scss';
 
-const SinglePagePhoto = (props) => {
-  return (
-    <div className='single-page-photo'>
-      <h1>Author: {props.singlePhoto.user.name}</h1>
-      <h4>Created: {new Date().toDateString(props.singlePhoto.created_at)}</h4>
-      <img src={props.singlePhoto.urls.thumb} alt={props.singlePhoto.user.name} />
-      <NavLink to ='/gallery'><i className="fas fa-arrow-alt-circle-left"></i></NavLink>
-    </div>
-  )
+class SinglePagePhoto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: false
+    }
+  }
+  render() {
+    return (
+      <div className='single-page-photo'>
+        <h1>Author: {this.props.singlePhoto.user.name}</h1>
+        <h4>Created: {new Date().toDateString(this.props.singlePhoto.created_at)}</h4>
+        <div className='img-wrapper'>
+          <img src={this.props.singlePhoto.urls.thumb} alt={this.props.singlePhoto.user.name} />
+          <i className="fas fa-star"></i>
+        </div>
+        <NavLink to='/gallery'><i className="fas fa-arrow-alt-circle-left"></i></NavLink>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
   const idSinglePhoto = ownProps.match.params.id;
-  const singlePhoto = state.gallery.photos.find((item)=>idSinglePhoto===item.id);
-  console.log(singlePhoto);
+  const singlePhoto = state.gallery.photos.find((item) => idSinglePhoto === item.id);
   return {
     singlePhoto
   }
