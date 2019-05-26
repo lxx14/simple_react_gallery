46d97bed415d097fab8b339556ca0f0fbf79dc22
lxx14 <lxx1427@gmail.com>
2019-05-26 16:23:10 +0300

    modificate service.js

--- src/components/Gallery/service.js
+++ src/components/Gallery/service.js
@@ -1,6 +1,9 @@
-import serverUrl from '../../system/serverUrlPhotos.json';
+const client_id = `ca5a2a324ba06f2cf8bede88a989bb6c2f5f87730032b3c6256b72888f2cc94c`; //ключ клиента
+const base_url = `https://api.unsplash.com/`; //неизменная часть адреса, у которого спрашиваем
 
+export const getPhotosFromServer = (page=1, per_page=20) => { //указываем что можем менять количество на странице и номер страницы и задаем их по умолчанию
 
-export const getPhotosFromServer = () => {
-  return fetch(serverUrl.url).then((responce) => {return responce.json()})
+  const url = `${base_url}photos/?client_id=${client_id}&&page=${page}&per_page=${per_page}`; //собираем нашу ссылку в кучу
+
+  return fetch(url).then((responce) => {return responce.json()});
 }
